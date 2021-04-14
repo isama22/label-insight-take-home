08f3873047e45f1bd20c0ec187acf86ae2261aa8
isama22 <emarti17@artic.edu>
2021-04-13 19:38:54 -0500

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom'
-
-// const App = () => {
-
-//   const [photos, setPhotos] = useState([])
-
-//   useEffect(() => {
-//     getPhotos();
-//   }, [])
-
-//   async function getPhotos() {
-//     const response = await fetch('https://jsonplaceholder.typicode.com/photos/?_limit=25')
-//     const photos = await response.json();
-//     setPhotos(photos)
-//   }
-
-//   return (
-//     <div className="App">
-//       <div className="items">
-//         <div className="grid">
-//           {photos.map(photo => (
-//             <Link to="/detail">
-//               <div className="grid-item">
-//                 <p>{photo.title}</p>
-//                 <img src={photo.thumbnailUrl} className="img" />
-//               </div>
-//             </Link>
-
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import './App.css';
 import Photos from './Photos'
+import PhotoDetail from './PhotoDetail'
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return(
     <div>
       <Photos />
+      <Switch>
+        <Route path="/detail" render={() => ( <PhotoDetail />)} />
+
+      </Switch>
     </div>
   )
 }
